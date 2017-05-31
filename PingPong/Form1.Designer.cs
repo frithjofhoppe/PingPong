348f40444164c8cb3ee6751ca4d3168fb52fd5a2
frithjofhoppe <frithjof.hoppe@icloud.com>
2017-05-31 11:19:39 +0200

    Projektdateien hinzufügen.

---
+++ PingPong/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace PingPong
+{
+    partial class frmPingPong
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlSpiel = new System.Windows.Forms.Panel();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tmrSpiel = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pnlSpiel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSpiel
+            // 
+            this.pnlSpiel.BackColor = System.Drawing.Color.SeaGreen;
+            this.pnlSpiel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSpiel.Controls.Add(this.picBall);
+            this.pnlSpiel.Location = new System.Drawing.Point(16, 15);
+            this.pnlSpiel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlSpiel.Name = "pnlSpiel";
+            this.pnlSpiel.Size = new System.Drawing.Size(1077, 650);
+            this.pnlSpiel.TabIndex = 0;
+            this.pnlSpiel.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlSpiel_Paint);
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.picBall.Location = new System.Drawing.Point(47, 53);
+            this.picBall.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(33, 31);
+            this.picBall.TabIndex = 1;
+            this.picBall.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(376, 1082);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(205, 53);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Spiel starten";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tmrSpiel
+            // 
+            this.tmrSpiel.Interval = 10;
+            this.tmrSpiel.Tick += new System.EventHandler(this.tmrSpiel_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(761, 1082);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(205, 53);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Spiel stoppen";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(1144, 1082);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(205, 53);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Schliessen";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmPingPong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 703);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlSpiel);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmPingPong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping-Pong Spiel";
+            this.Load += new System.EventHandler(this.frmPingPong_Load);
+            this.pnlSpiel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSpiel;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer tmrSpiel;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ PingPong/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPong
+{
+    public partial class frmPingPong : Form
+    {
+        int WertX = 5;
+        int WertY = 2;
+        public frmPingPong()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrSpiel.Start();
+        }
+
+        private void tmrSpiel_Tick(object sender, EventArgs e)
+        {
+             picBall.Location = new Point(picBall.Location.X+WertX, picBall.Location.Y+WertY);
+
+            if(picBall.Location.X>=pnlSpiel.Width-picBall.Width)
+            {
+                WertX = -WertX;
+            }
+
+            else if(picBall.Location.Y>=pnlSpiel.Height-picBall.Height)
+            {
+                WertY = -WertY;
+            }
+
+            else if (picBall.Location.X <= 0)
+            {
+                WertX = -WertX;
+            }
+
+            else if (picBall.Location.Y <= 0)
+            {
+                WertY = -WertY;
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmrSpiel.Stop();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pnlSpiel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frmPingPong_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
